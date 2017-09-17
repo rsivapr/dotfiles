2c2cd4037496664bfcc477db4796dc6d178602ec
Adam Boscarino <ajboscarino@gmail.com>
2017-09-17 15:33:53 -0400

    cleanup alias

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -39,3 +39,6 @@ alias gpub='git push -u origin $(git branch-name)'
 # Status/Logging
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+
+# Local Cleanup
+alias gcup='gup & gpub & git-local-cleanup'
