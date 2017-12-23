7930e0634ad6dafbd87d133dddb29daa88a4cdd1
Adam Boscarino <ajboscarino@gmail.com>
2017-12-23 12:52:38 -0500

    update atom and python stuff

--- python/install.sh
+++ python/install.sh
@@ -1,11 +1,10 @@
-pip3 install pipsi
-
-pipsi install pew
-pipsi install pipenv
-pipsi install ansible
-pipsi install awscli
-pipsi install twine
-pipsi install pyyaml
-pipsi install flake8
-pipsi install pycodestyle
-pipsi install yapf
+pip3 install --upgrade pew
+pip3 install --upgrade pipenv
+pip3 install --upgrade ansible
+pip3 install --upgrade awscli
+pip3 install --upgrade twine
+pip3 install --upgrade pyyaml
+pip3 install --upgrade flake8
+pip3 install --upgrade pycodestyle
+pip3 install --upgrade yapf
+pip3 install --upgrade python-language-server
