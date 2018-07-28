167168c800b676e3f012082021ecefe665938339
Adam Boscarino <ajboscarino@gmail.com>
2018-07-28 06:45:11 -0400

    python 3.7

--- python/install.sh
+++ python/install.sh
@@ -1,11 +1,13 @@
 # Setup pyenv
-pyenv install 3.6.4
+pyenv install 3.7.0
+pyenv install 3.6.6
 pyenv install 3.4.3
 pyenv install 2.7.14
-pyenv global 3.6.4
+pyenv global 3.7.0
 eval "$(pyenv init -)"
 
 # Install utility packages
+pip3 install --upgrade pip
 pip3 install --upgrade virtualenv
 pip3 install --upgrade pipenv
 pip3 install --upgrade ansible
@@ -14,4 +16,5 @@ pip3 install --upgrade twine
 pip3 install --upgrade pyyaml
 pip3 install --upgrade black
 pip3 install --upgrade pycodestyle
+pip3 install --upgrade pylint
 pip3 install --upgrade python-language-server
